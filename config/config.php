4417382ec7b4ee03c533f8f103a56ed2f1257629
= <=>
2020-01-23 15:16:05 +0100

    Add details to the config files

--- config/config.php
+++ config/config.php
@@ -4,5 +4,8 @@
  * You can place your custom package configuration in here.
  */
 return [
+    'token' => env('ESTORE_TOKEN'),
+    'email' => env('ESTORE_EMAIL'),
+    'username' => env('ESTORE_USERNAME'),
 
 ];
